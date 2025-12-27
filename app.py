b3ed2e405c26a103cb9b94446dc5d9bc0f85cec1
gladiatordan <dholland@theboys-db.com>
2025-12-27 03:18:54 -0800

    fix import error

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, jsonify, request, render_template, redirect, url_for, s
 from flask_cors import CORS
 
 # Local Imports
-from core.ipc import get_client, create_packet
+from SWGBuddy.core.ipc import get_client, create_packet
 
 
 app = Flask(__name__)
